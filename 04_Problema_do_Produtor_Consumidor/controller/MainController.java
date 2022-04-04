6e0e038f389c230880594099560f906b19a8da16
Viddernas <84942319+LucaslPedrosa@users.noreply.github.com>
2022-04-04 19:47:18 -0300

    Nah

---
+++ 04_Problema_do_Produtor_Consumidor/controller/MainController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController {
+
+    @FXML
+    private AnchorPane anchorPaneManagement;
+
+    @FXML
+    private ImageView imgShop;
+
+    @FXML
+    private ImageView imgProduction;
+
+    @FXML
+    private ImageView imgConsumer;
+
+    @FXML
+    private Label textLabelMoney;
+
+}
