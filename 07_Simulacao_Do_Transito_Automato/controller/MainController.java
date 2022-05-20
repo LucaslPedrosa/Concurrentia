f2eb8d66f3e66b9d9afa41aee0a59ad608ff7e43
Viddernas <84942319+LucaslPedrosa@users.noreply.github.com>
2022-05-20 19:37:57 -0300

    TODO: Sleep a little bit

--- 07_Simulacao_Do_Transito_Automato/controller/MainController.java
+++ 07_Simulacao_Do_Transito_Automato/controller/MainController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
@@ -54,33 +55,36 @@ public class MainController implements Initializable {
   @FXML
   private Slider speedSlider;
 
+  @FXML
+  private Label attLabel;
+
+  @FXML
+  private Label spdLabel;
+
+  @FXML
+  private Label vitLabel;
+
+  @FXML
+  private Label defLabel;
+
+  @FXML
+  private Label dexLabel;
+
+  @FXML
+  private Label wisLabel;
+
+  @FXML
+  private Label charText;
+
+  @FXML
+  private Button pauseButton;
+
   private int classSelected = 1;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     Variables.setUpSemaphores();
-    createThreads();
 
-    speedSlider.setOnDragOver(event -> {
-      switch (classSelected) {
-        case (1):
-          break;
-        case (2):
-          break;
-
-        default:
-          break;
-      }
-    });
-
-    warriorImg.setOnMouseClicked(event -> {
-      selectedImg.setImage(new Image("/img/characters/warrior.png"));
-      classSelected = 1;
-    });
-
-  }
-
-  public void createThreads() {
     Warrior warrior = new Warrior("Warrior", warriorImg);
     warrior.start();
 
@@ -101,6 +105,173 @@ public class MainController implements Initializable {
 
     Paladin paladin = new Paladin("Paladin", paladinImg);
     paladin.start();
+
+    speedSlider.setOnMouseDragged(event -> {
+      switch (classSelected) {
+        case (1):
+          warrior.setSpeed((int) Math.round(speedSlider.getValue()));
+          System.out.println(Math.round(speedSlider.getValue()));
+          break;
+        case (2):
+          archer.setSpeed((int) Math.round(speedSlider.getValue()));
+          System.out.println(Math.round(speedSlider.getValue()));
+          break;
+        case (3):
+          knight.setSpeed((int) Math.round(speedSlider.getValue()));
+          System.out.println(Math.round(speedSlider.getValue()));
+          break;
+        case (4):
+          mage.setSpeed((int) Math.round(speedSlider.getValue()));
+          System.out.println(Math.round(speedSlider.getValue()));
+          break;
+        case (5):
+          ninja.setSpeed((int) Math.round(speedSlider.getValue()));
+          System.out.println(Math.round(speedSlider.getValue()));
+          break;
+        case (6):
+          rogue.setSpeed((int) Math.round(speedSlider.getValue()));
+          System.out.println(Math.round(speedSlider.getValue()));
+          break;
+        case (7):
+          paladin.setSpeed((int) Math.round(speedSlider.getValue()));
+          System.out.println(Math.round(speedSlider.getValue()));
+          break;
+        default:
+          break;
+      }
+    });
+
+    pauseButton.setOnAction(event -> {
+      switch (classSelected) {
+        case (1):
+          warrior.changePause();
+          break;
+        case (2):
+          archer.changePause();
+          break;
+        case (3):
+          knight.changePause();
+          break;
+        case (4):
+          mage.changePause();
+          break;
+        case (5):
+          ninja.changePause();
+          break;
+        case (6):
+          rogue.changePause();
+          break;
+        case (7):
+          paladin.changePause();
+          break;
+
+      }
+    });
+
+    warriorImg.setOnMouseClicked(event -> {
+      selectedImg.setImage(new Image("/img/characters/warrior.png"));
+      selectedText.setText("Warrior");
+      classSelected = 1;
+      charText.setText(
+          "A classe mais forte do jogo, Warrior possui o maior ataque do jogo alem de alta defesa e poder 'buffar' sua velocidade e destreza");
+      attLabel.setText("75");
+      defLabel.setText("69");
+      spdLabel.setText("50");
+      dexLabel.setText("50");
+      vitLabel.setText("75");
+      wisLabel.setText("50");
+      routeImg.setImage(new Image("/img/routes/warRoute.png"));
+    });
+
+    archerImg.setOnMouseClicked(event -> {
+      selectedImg.setImage(new Image("/img/characters/archer.png"));
+      selectedText.setText("Archer");
+      classSelected = 2;
+      charText.setText(
+          "Uma das classes mais fracas do jogo, o arqueiro possui ataque fraco devido a sua arma e sua utilidade e bem limitada");
+      attLabel.setText("75");
+      defLabel.setText("42");
+      spdLabel.setText("50");
+      dexLabel.setText("50");
+      vitLabel.setText("50");
+      wisLabel.setText("50");
+      routeImg.setImage(new Image("/img/routes/archerRoute.png"));
+    });
+
+    knightImg.setOnMouseClicked(event -> {
+      selectedImg.setImage(new Image("/img/characters/knight.png"));
+      selectedText.setText("Knight");
+      classSelected = 3;
+      charText.setText(
+          "A classe mais facil de se jogar, a sua alta defesa e vida permita com que voce nao morra com facilidade");
+      attLabel.setText("50");
+      defLabel.setText("80");
+      spdLabel.setText("50");
+      dexLabel.setText("50");
+      vitLabel.setText("75");
+      wisLabel.setText("50");
+      routeImg.setImage(new Image("/img/routes/knightRoute.png"));
+    });
+
+    mageImg.setOnMouseClicked(event -> {
+      selectedImg.setImage(new Image("/img/characters/mage.png"));
+      selectedText.setText("Mage");
+      classSelected = 4;
+      charText.setText(
+          "A segunda classe mais forte do jogo, alto dano e distancia de ataque permite com que voce jogue tranquilo");
+      attLabel.setText("75");
+      defLabel.setText("35");
+      spdLabel.setText("50");
+      dexLabel.setText("75");
+      vitLabel.setText("50");
+      wisLabel.setText("75");
+      routeImg.setImage(new Image("/img/routes/mageRoute.png"));
+    });
+
+    ninjaImg.setOnMouseClicked(event -> {
+      selectedImg.setImage(new Image("/img/characters/ninja.png"));
+      selectedText.setText("Ninja");
+      classSelected = 5;
+      charText.setText(
+          "A pior classe do jogo, suas habilidades nao ajudam o coletivo, nao possui dano alto e taopouco defesa");
+      attLabel.setText("75");
+      defLabel.setText("42");
+      spdLabel.setText("75");
+      dexLabel.setText("75");
+      vitLabel.setText("50");
+      wisLabel.setText("50");
+      routeImg.setImage(new Image("/img/routes/ninjaRoute.png"));
+    });
+
+    rogueImg.setOnMouseClicked(event -> {
+      selectedImg.setImage(new Image("/img/characters/rogue.png"));
+      selectedText.setText("Rogue");
+      classSelected = 6;
+      charText.setText(
+          "Uma classe extremamente individual e de baixo dano, entretanto sua capacidade de ficar invisivel ajuda o grupo em momentos especificos");
+      attLabel.setText("50");
+      defLabel.setText("42");
+      spdLabel.setText("75");
+      dexLabel.setText("75");
+      vitLabel.setText("50");
+      wisLabel.setText("60");
+      routeImg.setImage(new Image("/img/routes/rogueRoute.png"));
+    });
+
+    paladinImg.setOnMouseClicked(event -> {
+      selectedImg.setImage(new Image("/img/characters/paladin.png"));
+      selectedText.setText("Paladin");
+      classSelected = 7;
+      charText.setText(
+          "Nao e das mais fortes, mas e a segunda mais necessaria, pode curar os aliados e aumentar o seu dano");
+      attLabel.setText("50");
+      defLabel.setText("52");
+      spdLabel.setText("50");
+      dexLabel.setText("45");
+      vitLabel.setText("50");
+      wisLabel.setText("75");
+      routeImg.setImage(new Image("/img/routes/paladinRoute.png"));
+    });
 
   }
 
--- 07_Simulacao_Do_Transito_Automato/model/BaseCharacter.java
+++ 07_Simulacao_Do_Transito_Automato/model/BaseCharacter.java
@@ -1,54 +1,82 @@
 package model;
 
+import java.util.concurrent.Semaphore;
+
 import javafx.application.Platform;
 import javafx.scene.image.ImageView;
 
-public class BaseCharacter extends Thread{
+public class BaseCharacter extends Thread {
 
   private String name;
   private double x;
   private double y;
   private ImageView img;
   private int speed = 5;
+  private Semaphore isActive = new Semaphore(1);
+  private boolean isRunning = true;
 
-  public BaseCharacter(String name,ImageView img){
+  public BaseCharacter(String name, ImageView img) {
     this.name = name;
     this.img = img;
     this.x = img.getX();
     this.y = img.getY();
   }
 
-  public void moveX(int amount){
-    double toSum = (amount > 1? 1 : -1);
+  public void moveX(int amount) {
+    double toSum = (amount > 1 ? 1 : -1);
     double count = 0;
 
-    while(count != amount){
-
+    while (count != amount) {
+      acActive();
       setX(getX() + toSum);
       Platform.runLater(() -> {
         img.setX(getX());
       });
       count += toSum;
       toSleeep(getSpeed());
+      relActive();
     }
   }
 
-  public void moveY(int amount){
-    double toSum = (amount > 1? 1 : -1);
+  public void moveY(int amount) {
+    double toSum = (amount > 1 ? 1 : -1);
     double count = 0;
 
-    while(count != amount){
-
+    while (count != amount) {
+      acActive();
       setY(getY() + toSum);
       Platform.runLater(() -> {
         img.setY(getY());
       });
       count += toSum;
       toSleeep(getSpeed());
+      relActive();
     }
   }
 
-  public void toSleeep(int miliseconds){
+  public void changePause(){
+    if(isRunning)
+      acActive();
+    else
+      relActive();
+    changeRunning();
+  }
+
+  public void acActive() {
+    try {
+      isActive.acquire();
+    } catch (Exception e) {
+    }
+  }
+
+  public void relActive() {
+    try {
+      isActive.release();
+    } catch (Exception e) {
+    }
+  }
+
+  public void toSleeep(int miliseconds) {
     try {
       Thread.sleep(miliseconds);
     } catch (Exception e) {
@@ -57,32 +85,40 @@ public class BaseCharacter extends Thread{
   }
 
   @Override
-  public void run(){
+  public void run() {
     System.out.println("eae");
   }
 
-  public String getNeime(){
+  public String getNeime() {
     return this.name;
   }
 
-  public double getX(){
+  public double getX() {
     return this.x;
   }
 
-  public double getY(){
+  public double getY() {
     return this.y;
   }
 
-  public void setY(double y){
+  public void setY(double y) {
     this.y = y;
   }
 
-  public void setX(double x){
+  public void setX(double x) {
     this.x = x;
   }
 
-  public int getSpeed(){
+  public int getSpeed() {
     return this.speed;
   }
-  
+
+  public void setSpeed(int spd){
+    this.speed = spd;
+  }
+
+  public void changeRunning(){
+    this.isRunning = !isRunning;
+  }
+
 }
